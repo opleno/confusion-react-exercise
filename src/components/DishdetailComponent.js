d7f8ec30db919766269d742263fe5e25fc6b2e20
Alex Cuenca-Parra <alejandrocuencaparra@gmail.com>
2022-09-19 11:42:10 +0200

    Single Page Applications Part 2
    Signed-off-by: Alex Cuenca-Parra <alejandrocuencaparra@gmail.com>

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,63 +1,84 @@
 import React from "react";
-import { Card, CardImg, CardText, CardBody, CardTitle } from "reactstrap";
+import {
+  Card,
+  CardImg,
+  CardText,
+  CardBody,
+  CardTitle,
+  Breadcrumb,
+  BreadcrumbItem,
+} from "reactstrap";
+import { Link } from "react-router-dom";
 
-  function RenderDish({dish}) {
-    if (dish != null)
+function RenderDish({ dish }) {
+  if (dish != null)
+    return (
+      <Card key={dish.id}>
+        <CardImg width="100%" src={dish.image} alt={dish.name} />
+        <CardBody>
+          <CardTitle>{dish.name}</CardTitle>
+          <CardText>{dish.description}</CardText>
+        </CardBody>
+      </Card>
+    );
+  else return <div></div>;
+}
+
+function RenderComments({ comments }) {
+  if (comments != null) {
+    const commentList = comments.map((comment) => {
+      let date = new Intl.DateTimeFormat("en-US", {
+        year: "numeric",
+        month: "short",
+        day: "2-digit",
+      }).format(new Date(Date.parse(comment.date)));
       return (
-        <Card key={dish.id}>
-          <CardImg width="100%" src={dish.image} alt={dish.name} />
-          <CardBody>
-            <CardTitle>{dish.name}</CardTitle>
-            <CardText>{dish.description}</CardText>
-          </CardBody>
-        </Card>
+        <li key={comment.id}>
+          <CardText>{comment.comment}</CardText>
+          <CardText>
+            -- {comment.author} , {date}
+          </CardText>
+          <p></p>
+        </li>
       );
-    else return <div></div>;
-  }
+    });
 
-  function RenderComments({comments}) {
-    if (comments != null) {
-      const commentList = comments.map((comment) => {
-        let date = new Intl.DateTimeFormat("en-US", {
-          year: "numeric",
-          month: "short",
-          day: "2-digit",
-        }).format(new Date(Date.parse(comment.date)));
-        return (
-          <li key={comment.id}>
-            <CardText>{comment.comment}</CardText>
-            <CardText>
-              -- {comment.author} , {date}
-            </CardText>
-            <p></p>
-          </li>
-        );
-      });
+    return (
+      <div className="list-unstyled">
+        <h4>Comments</h4>
+        {commentList}
+      </div>
+    );
+  } else return <div></div>;
+}
 
-      return (
-        <div className="list-unstyled">
-          <h4>Comments</h4>
-          {commentList}
-        </div>
-      );
-    } else return <div></div>;
-  }
-
-  const DishDetail = (props) => {
-    if (props.dish != null)
-      return (
-        <div className="container">
-          <div className="row">
-            <div className="col-12 col-md-5 m-1">
-              <RenderDish dish={props.dish}/>
-            </div>
-            <div className="col-12 col-md-5 m-1">
-              <RenderComments comments={props.dish.comments} />
-            </div>
+const DishDetail = (props) => {
+  if (props.dish != null)
+    return (
+      <div className="container">
+        <div className="row">
+          <Breadcrumb>
+            <BreadcrumbItem>
+              <Link to="/menu">Menu</Link>
+            </BreadcrumbItem>
+            <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+          </Breadcrumb>
+          <div className="col-12">
+            <h3>{props.dish.name}</h3>
+            <hr />
           </div>
         </div>
-      );
-    else return <div></div>;
-  }
+        <div className="row">
+          <div className="col-12 col-md-5 m-1">
+            <RenderDish dish={props.dish} />
+          </div>
+          <div className="col-12 col-md-5 m-1">
+            <RenderComments comments={props.comments} />
+          </div>
+        </div>
+      </div>
+    );
+  else return <div></div>;
+};
 
 export default DishDetail;
