d4f880cbdb37edeca9b16ba45d070013a365ce80
Alex Cuenca-Parra <alejandrocuencaparra@gmail.com>
2022-09-25 14:17:00 +0200

    Redux Thunk
    Signed-off-by: Alex Cuenca-Parra <alejandrocuencaparra@gmail.com>

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -10,7 +10,7 @@ import Home from "./HomeComponent";
 import Contact from "./ContactComponent";
 import AboutComponent from "./AboutComponent";
 
-import { addComment } from "../redux/ActionCreators";
+import { addComment, fetchDishes } from "../redux/ActionCreators";
 
 const mapStateToProps = (state) => {
   return {
@@ -24,14 +24,25 @@ const mapStateToProps = (state) => {
 const mapDispatchToProps = (dispatch) => ({
   addComment: (dishId, rating, author, comment) =>
     dispatch(addComment(dishId, rating, author, comment)),
+  fetchDishes: () => {
+    dispatch(fetchDishes());
+  },
 });
 
 class Main extends Component {
+  componentDidMount() {
+    this.props.fetchDishes();
+  }
+
   render() {
     const HomePage = () => {
       return (
         <Home
-          dish={this.props.dishes.filter((dish) => dish.featured === true)[0]}
+          dish={
+            this.props.dishes.dishes.filter((dish) => dish.featured === true)[0]
+          }
+          dishesLoading={this.props.dishes.isLoading}
+          dishesErrMess={this.props.dishes.errMess}
           promotion={
             this.props.promotions.filter((promo) => promo.featured === true)[0]
           }
@@ -46,10 +57,12 @@ class Main extends Component {
       return (
         <DishDetail
           dish={
-            this.props.dishes.filter(
+            this.props.dishes.dishes.filter(
               (dish) => dish.id === parseInt(match.params.dishId, 10)
             )[0]
           }
+          isLoading={this.props.dishes.isLoading}
+          errMess={this.props.dishes.errMess}
           comments={this.props.comments.filter(
             (comment) => comment.dishId === parseInt(match.params.dishId, 10)
           )}
